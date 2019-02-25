a20ab744189f26aeb3c9bfaa2abff21a518c9b46
Walt Javins <walt@javins.net>
2019-02-25 12:29:07 -0800

    Add initial test cases for PD001.
    Not sure these are perfect (particularly in the ast binding) but we'll
    give them a shot.

---
+++ test/test_PD001.py
@@ -0,0 +1,38 @@
+# stdlib
+import ast
+
+from pandas_vet import VetPlugin
+from pandas_vet import PD001
+
+
+def test_PD001_pass():
+    """
+    Test that importing pandas the recommended way does not result in an error.
+    """
+    statement = "import pandas as pd"
+    tree = ast.parse(statement)
+    actual = list(VetPlugin(tree).run())
+    expected = []
+    assert actual == expected
+
+
+def test_PD001_fail_no_as():
+    """
+    Test that importing pandas without an 'as' clause results in an error.
+    """
+    statement = "import pandas"
+    tree = ast.parse(statement)
+    actual = list(VetPlugin(tree).run())
+    expected = [PD001(1, 1)]
+    assert actual == expected
+
+
+def test_PD001_fail_wrong_alias():
+    """
+    Test that importing pandas with an odd name results in an error.
+    """
+    statement = "import pandas as foo"
+    tree = ast.parse(statement)
+    actual = list(VetPlugin(tree).run())
+    expected = [PD001(1, 1)]
+    assert actual == expected
