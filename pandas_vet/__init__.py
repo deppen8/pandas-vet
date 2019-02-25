8a6522044f3d3deaaf902d3893712daff19383fc
Walt Javins <walt@javins.net>
2019-02-25 12:28:04 -0800

    Add PD001 error for bad imports.
    We always want folks to `import pandas as pd`. PD001 should result if
    they do it any other way.

--- pandas_vet/__init__.py
+++ pandas_vet/__init__.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+from functools import partial
+
+
 class VetPlugin:
     name = "flake8-pandas-vet"
     version = "0.1.0"
@@ -8,3 +12,12 @@ class VetPlugin:
     def run(self):
         for message in []:
             yield message
+
+
+error = namedtuple("Error", ["lineno", "col", "message", "type"])
+Error = partial(partial, error, type=VetPlugin)
+
+
+PD001 = Error(
+    "pandas should always be imported as 'import pandas as pd'"
+)
