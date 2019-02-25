157d39c4341d426f6bc68dcfc36b4bb76d8049c6
Jacob Deppen <deppen.8@gmail.com>
2019-02-25 12:54:35 -0800

    add error codes PD002-PD006

--- pandas_vet/__init__.py
+++ pandas_vet/__init__.py
@@ -21,3 +21,18 @@ Error = partial(partial, error, type=VetPlugin)
 PD001 = Error(
     "pandas should always be imported as 'import pandas as pd'"
 )
+PD002 = Error(
+    "'inplace = True' should be avoided; it has inconsistent behavior"
+)
+PD003 = Error(
+    "'.isna' is preferred to '.isnull'; functionality is equivalent"
+)
+PD004 = Error(
+    "'.notna' is preferred to '.notnull'; functionality is equivalent"
+)
+PD005 = Error(
+    "Use arithmetic operator instead of method"
+)
+PD006 = Error(
+    "Use comparison operator instead of method"
+)
