f4b6f842786e553f5eb1d77133667039905b1ea6
simchuck <charles.simchick@gmail.com>
2019-02-26 12:21:29 -0800

    Refactor tests to group by error number.
    Closes #26.  Separate modules for each linter error number, including all corresponding test signatures.

---
+++ tests/test_PD004.py
@@ -0,0 +1,29 @@
+# stdlib
+import ast
+
+from pandas_vet import VetPlugin
+from pandas_vet import PD004
+
+
+def test_PD004_pass():
+    """
+    Test that using .notna() explicitly does not result in an error.
+    """
+    statement = "notnas = pd.notna(val)"
+    tree = ast.parse(statement)
+    actual = list(VetPlugin(tree).run())
+    expected = []
+    assert actual == expected
+
+
+def test_PD004_fail():
+    """
+    Test that using .notnull() results in an error.
+    """
+    statement = "notnulls = pd.notnull(val)"
+    tree = ast.parse(statement)
+    actual = list(VetPlugin(tree).run())
+    expected = [PD004(1, 11)]
+    assert actual == expected
+
+
