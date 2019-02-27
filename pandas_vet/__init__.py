a0d05d124b392d570fbf27a4a084c049af107387
simchuck <charles.simchick@gmail.com>
2019-02-27 12:25:17 -0800

    Fix '.ix' check for ast.Subscript
    Includes corrected syntax for test statements, based on pandas documentation.

--- pandas_vet/__init__.py
+++ pandas_vet/__init__.py
@@ -26,6 +26,9 @@ class Visitor(ast.NodeVisitor):
         self.errors.extend(check_inplace_false(node))
         self.errors.extend(check_for_isnull(node))
         self.errors.extend(check_for_notnull(node))
+
+    def visit_Subscript(self, node):
+        self.generic_visit(node)  # continue checking children
         self.errors.extend(check_for_ix(node))
 
     def check(self, node):
@@ -75,9 +78,9 @@ def check_for_notnull(node: ast.Call) -> List:
     return errors
 
 
-def check_for_ix(node: ast.Call) -> List:
+def check_for_ix(node: ast.Subscript) -> List:
     errors = []
-    if node.func.attr == "ix":
+    if node.value.attr == "ix":
         errors.append(PD007(node.lineno, node.col_offset))
     return errors
 
--- tests/test_PD007.py
+++ tests/test_PD007.py
@@ -9,7 +9,7 @@ def test_PD007_pass_loc():
     """
     Test that using .loc() explicitly does not result in an error.
     """
-    statement = "index = pd.loc(val)"
+    statement = "new_df = df.loc['d':, 'A':'C']"
     tree = ast.parse(statement)
     actual = list(VetPlugin(tree).run())
     expected = []
@@ -20,7 +20,7 @@ def test_PD007_pass_iloc():
     """
     Test that using .iloc() explicitly does not result in an error.
     """
-    statement = "index = pd.iloc(val)"
+    statement = "new_df = df.iloc[[1, 3, 5], [1, 3]]"
     tree = ast.parse(statement)
     actual = list(VetPlugin(tree).run())
     expected = []
@@ -31,10 +31,10 @@ def test_PD007_fail():
     """
     Test that using .ix() results in an error.
     """
-    statement = "index = pd.ix(val)"
+    statement = "s = df.ix[[0, 2], 'A']"
     tree = ast.parse(statement)
     actual = list(VetPlugin(tree).run())
-    expected = [PD007(1, 8)]
+    expected = [PD007(1, 4)]
     assert actual == expected
 
 
