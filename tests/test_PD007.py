c6e8a3d4f32a6a97c5015f55c720ff4a04b787b6
simchuck <charles.simchick@gmail.com>
2019-02-27 15:06:57 -0800

    Fix syntax in test docstrings.

--- tests/test_PD007.py
+++ tests/test_PD007.py
@@ -7,7 +7,7 @@ from pandas_vet import PD007
 
 def test_PD007_pass_loc():
     """
-    Test that using .loc() explicitly does not result in an error.
+    Test that using .loc[] explicitly does not result in an error.
     """
     statement = "new_df = df.loc['d':, 'A':'C']"
     tree = ast.parse(statement)
@@ -18,7 +18,7 @@ def test_PD007_pass_loc():
 
 def test_PD007_pass_iloc():
     """
-    Test that using .iloc() explicitly does not result in an error.
+    Test that using .iloc[] explicitly does not result in an error.
     """
     statement = "new_df = df.iloc[[1, 3, 5], [1, 3]]"
     tree = ast.parse(statement)
@@ -29,7 +29,7 @@ def test_PD007_pass_iloc():
 
 def test_PD007_fail():
     """
-    Test that using .ix() results in an error.
+    Test that using .ix[] results in an error.
     """
     statement = "s = df.ix[[0, 2], 'A']"
     tree = ast.parse(statement)
