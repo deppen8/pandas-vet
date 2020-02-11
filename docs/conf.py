a4416255c7b0dd10daec2fea6429604af4b83c00
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-02-10 22:04:44 -0800

    sphinx: use the Read the Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ release = '0.2.2'
 # ones.
 extensions = [
     'autoapi.extension',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
