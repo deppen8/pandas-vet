c03295e3b093a499217df8953433f6c94af91ac1
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-02-10 16:10:31 -0800

    sphinx: initial autodoc configuration
    This isn't the final location of the API docs, but it's proof of
    concept.

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,6 +31,7 @@ release = '0.2.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +54,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -10,6 +10,8 @@ Welcome to pandas-vet's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+.. automodule:: pandas_vet
+    :members:
 
 
 Indices and tables
