1af94e14c9f96e173dec8f08b7b8268d61225c7c
Kasper Primdal Lauritzen <git@kplauritzen.dk>
2022-08-05 14:38:36 +0200

    fix(pd002): Avoid crashing with inplace and variable
    Closes #111
    
    Fix issue where flake8 would crash when analysing an "inplace=<some variable>" statement.
    
    Adds test to avoid regression

--- tests/test_PD002.py
+++ tests/test_PD002.py
@@ -25,3 +25,16 @@ def test_PD002_fail():
     actual = list(VetPlugin(tree).run())
     expected = [PD002(1, 0)]
     assert actual == expected
+
+
+def test_PD002_with_variable_does_not_crash():
+    """
+    Test that using inplace=<some variable> does not raise Exceptions.
+
+    It will not be able to infer the value of the variable, so no errors either.
+    """
+    statement = """use_inplace=True; df.drop(['a'], axis=1, inplace=use_inplace)"""
+    tree = ast.parse(statement)
+    actual = list(VetPlugin(tree).run())
+    expected = []
+    assert actual == expected
